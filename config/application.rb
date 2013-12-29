cb1eb8eb845af86eaf1ba589a0a380a5d0053d72
Dominic Smith <dominic.smith2@gmail.com>
2013-12-28 23:22:44 -0500

    Added CSS to the application controller by integrating bootstrap-css. Now the navigation (_header.html.erb) bar looks good across the top.  Still need to fix the show because the title is overlapping the top div.

--- config/application.rb
+++ config/application.rb
@@ -27,11 +27,7 @@ module Prelims
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.time_zone = 'Central Time (US & Canada)'
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
@@ -58,6 +54,7 @@ module Prelims
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     config.generators do |g| 
       g.test_framework :rspec, 
 	  :fixtures => true, 
