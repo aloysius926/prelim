e38dbdca7de5bf4e29fcd5fb584cee2768b32a41
Dominic Smith <dominic.smith2@gmail.com>
2013-12-29 20:28:34 -0500

    Finally remembered to migrate the answer_rating addition.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131229190121) do
+ActiveRecord::Schema.define(version: 20131229191405) do
+
+  create_table "answer_ratings", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "answer_id"
+    t.integer  "overall"
+    t.integer  "clarity"
+    t.integer  "detail"
+    t.integer  "correctness"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "answer_ratings", ["answer_id"], name: "index_answer_ratings_on_answer_id"
+  add_index "answer_ratings", ["user_id"], name: "index_answer_ratings_on_user_id"
 
   create_table "answers", force: true do |t|
     t.integer  "user_id"
