7e7a064888a991b5b2368f37ddd63e4a2db1a28d
Dominic Smith <dominic.smith2@gmail.com>
2014-01-06 17:56:36 -0500

    Added the ability to edit sittings if you know the url.

--- app/controllers/sittings_controller.rb
+++ app/controllers/sittings_controller.rb
@@ -14,6 +14,20 @@ class SittingsController < ApplicationController
   def new
     @sitting = @question.sittings.build
   end
+  def edit
+    @sitting = Sitting.find(params[:id])
+    @question = Question.find(params[:question_id])
+  end
+  def update
+    @sitting = Sitting.find(params[:id])
+    @question = Question.find(params[:question_id])
+    if @sitting.update_attributes(sitting_params)
+       flash[:success] = "Sitting updated"
+      redirect_to :root
+    else
+      render 'edit'
+    end
+  end
 end
 
 private
