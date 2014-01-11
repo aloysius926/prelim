1cfb47073dd0e8fa9b9f0f36c76a46bbb8126a20
Dominic Smith <dominic.smith2@gmail.com>
2014-01-11 09:27:35 -0500

    Added a default host to the production file.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Prelims::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  config.action_mailer.default_url_options = { :host => "fathomless-crag-2414.herokuapps.com" }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
