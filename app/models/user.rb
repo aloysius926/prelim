c8d5b62037c49fce6ac627b82f998cfac91c6ab9
Dominic Smith <dominic.smith2@gmail.com>
2014-01-11 09:44:16 -0500

    Making the email only validate on create so andrew can have his gmail.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   before_save {self.email = email.downcase }
   validates :name, presence: true, length: {maximum: 30}
   VALID_EMAIL_REGEX =  /\A[\w+\-.]+@umn.edu\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }, on: :create
   #validates :password, length: { minimum: 6 } :on => :create
   has_many :question_ratings
   has_many :answer_ratings
