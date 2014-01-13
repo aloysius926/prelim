a9eab23c56be799e81e095e81247ce0065882501
Dominic Smith <dominic.smith2@gmail.com>
2014-01-12 22:57:33 -0500

    Changed enforcing language locales.

--- config/application.rb
+++ config/application.rb
@@ -48,14 +48,14 @@ module Prelims
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     #config.active_record.whitelist_attributes = true
-    config.i18n.enforce_available_locales = false
+    config.i18n.enforce_available_locales = true
     # Enable the asset pipeline
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-    config.assets.precompile += %w['active_admin.css.scss', 'active_admin.js']
+    #config.assets.precompile += %w['active_admin.css.scss', 'active_admin.js']
     config.generators do |g| 
       g.test_framework :rspec, 
 	  :fixtures => true, 
