a4f1c223b3129ffe7a065104a6e73d2893c8f26e
Dominic Smith <dominic.smith2@gmail.com>
2014-01-16 12:14:52 -0500

    Centered Table Headers and added footnotes to the user table.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,16 +3,16 @@ module UsersHelper
     term_id = Term.where("term = :term", term: term).first.id
     @finished = FinishedQuestion.where("user_id = :user",user: uid).pluck(:question_id)
     @sittings = Sitting.where("year=:yr AND term_id=:term AND number=:number",yr: yr,term: term_id,number: number)
-    teacher,url = '',''
+    teacher,url,link_class = '','',''
     @sittings.each do |s|
       if s.question.subject.id == subject   
         if @finished.include?(s.question.id)
-          teacher,url =  'X','#'
+          teacher,url,link_class =  s.question.professor.name[0..0],question_path(s.question.id),'finished'
 	else
-	  teacher,url=   s.question.professor.name[0..0],question_path(s.question.id)
+	  teacher,url,link_class=   s.question.professor.name[0..0],question_path(s.question.id),'waiting'
         end
       end
     end
-    link_to(teacher,url)
+    link_to(teacher,url,class: link_class)
   end
 end   
