8dbfaf09b1e1808e85c8e656479868566bc9afe9
Dominic Smith <Dominic.Smith2@gmail.com>
2014-01-27 00:04:26 -0600

    Not letting users review their own answers anymore.

--- app/controllers/answer_ratings_controller.rb
+++ app/controllers/answer_ratings_controller.rb
@@ -1,9 +1,13 @@
 class AnswerRatingsController < ApplicationController
   def create
     @answer = Answer.find(params[:answer_id])
-    @answer_rating = @answer.answer_ratings.create(params[:answer_rating].permit(:overall, :detail, :clarity, :correctness,:comment))
+    @answer_rating = @answer.answer_ratings.new(params[:answer_rating].permit(:overall, :detail, :clarity, :correctness,:comment))
     @answer_rating.user = current_user
-    @answer_rating.save!
+    if current_user.id != @answer.user.id && @answer_rating.save
+    	flash[:notice] = "Rating Added"
+    else
+    	flash[:error] = 'Adding the rating failed'
+    end
     redirect_to answer_path(@answer)
   end
   def new
--- app/models/answer_rating.rb
+++ app/models/answer_rating.rb
@@ -1,5 +1,7 @@
+# Tracks answer Ratings
 class AnswerRating < ActiveRecord::Base
   belongs_to :user
   belongs_to :answer
+  has_many :answer_ratings, dependent: :destroy
   validates_uniqueness_of :answer_id, :scope => :user_id
 end
