2a74ba289cb8a87161761c79860255c2c4829d6c
Dominic Smith <Dominic.Smith2@gmail.com>
2014-01-31 22:28:20 -0600

    Added progress tracking to Summary Page.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -6,6 +6,25 @@ class StaticController < ApplicationController
   def summary
     @professors = Professor.order(:name)
     @finishedquestions = FinishedQuestion.user_finished(current_user.id)
+    professors = Professor.where("name in ('Allen','Werner','Rustichini','Kehoe, T.','Jones','Chari','Rios-Rull')").pluck(:id)
+    @micro_questions = Question.joins(:subject).where(:professor_id => professors).where("subjects.name = 'Micro'")
+    @micro_complete = @micro_questions.joins(:finished_questions).where("finished_questions.user_id = #{current_user.id} and
+    	                                                                 finished = TRUE").size
+    @micro_total_min = @micro_questions.size
+    @micro_total_max = @micro_total_min + Question.joins(:subject,:professor).where("subjects.name = 'Micro' and professors.name = 'UNKNOWN'").size
+    @micro_to_exam = (Date.new(2014, 5, 23) - Date.today).to_i
+    @micro_per_day = @micro_total_min.to_i * 1.0 / @micro_to_exam.to_i
+
+    # I really need to learn metaprogramming in rails
+    @macro_questions = Question.joins(:subject).where(:professor_id => professors).where("subjects.name = 'Macro'")
+    @macro_complete = @macro_questions.joins(:finished_questions).where("finished_questions.user_id = #{current_user.id} and
+    	                                                                 finished = TRUE").size
+    @macro_total_min = @macro_questions.size
+    @macro_total_max = @macro_total_min + Question.joins(:subject,:professor).where("subjects.name = 'Macro' and professors.name = 'UNKNOWN'").size
+    @macro_to_exam = (Date.new(2014, 5, 30) - Date.today).to_i
+    @macro_per_day = @macro_total_min.to_i * 1.0 / @macro_to_exam.to_i
+
+
   end
 
   def teacher
