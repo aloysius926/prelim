767f46ea1efb6385f648b30a050d37bc3a879d95
Dominic Smith <Dominic.Smith2@gmail.com>
2014-02-05 21:39:51 -0600

    Added the ability to edit answers.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,7 +2,7 @@
 class AnswersController < ApplicationController
   def create
     @question = Question.find(params[:question_id])
-    @answer = @question.answers.new(params[:answer].permit(:pdf, :typed))
+    @answer = @question.answers.new(answer_params)
     @answer.user_id = current_user.id
     if @answer.save
       flash[:notice] = 'Your answer has been uploaded.  Thanks a lot'
@@ -16,4 +16,24 @@ class AnswersController < ApplicationController
   def show
     @answer = Answer.find(params[:id])
   end
+
+  def edit
+    @answer = Answer.find(params[:id])
+  end
+
+  def update
+    @answer = Answer.find(params[:id])
+    @question = Question.find(answer.question.id)
+    if @answer.update_attributes(answer_params)
+      flash[:success] = "Answer Updated"
+      redirect_to question_path(@question)
+    else
+      render 'edit'
+    end
+  end
+end
+
+private
+def answer_params
+  params.require(:answer).premit(:pdf, :typed)
 end
